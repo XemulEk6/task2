985eacb8f79066fb45bbf24f21d44e2ede3d54fe
XemulEk6 <xemul.ek6@gmail.com>
2023-03-03 17:04:07 +0500

    input value

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//  2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int thirdValue = Convert.ToInt32(Console.ReadLine());
